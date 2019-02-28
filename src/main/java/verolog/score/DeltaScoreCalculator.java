0ee86bb37044d14c1380e1e477f3934cb3fee91c
Raúl Martín <r.martinsanta@alumnos.urjc.es>
2019-02-28 20:21:02 +0100

    jakuu

--- src/main/java/verolog/score/DeltaScoreCalculator.java
+++ src/main/java/verolog/score/DeltaScoreCalculator.java
@@ -1,24 +1,33 @@
 package verolog.score;
 
 import org.optaplanner.core.api.score.Score;
+import org.optaplanner.core.api.score.buildin.hardsoftlong.HardSoftLongScore;
 import org.optaplanner.core.impl.score.director.incremental.AbstractIncrementalScoreCalculator;
+import verolog.model.Anchor;
+import verolog.model.Slide;
 import verolog.model.Solution;
 
 public class DeltaScoreCalculator extends AbstractIncrementalScoreCalculator<Solution> {
 
+    long softscore;
+
+
     @Override
     public void resetWorkingSolution(Solution solution) {
-
+        this.softscore = 0;
     }
 
     @Override
     public void beforeEntityAdded(Object o) {
-
+        // nothing
     }
 
     @Override
     public void afterEntityAdded(Object o) {
-
+        if(!(o instanceof Slide)){
+            return;
+        }
+        insert((Slide) o);
     }
 
     @Override
@@ -33,16 +42,30 @@ public class DeltaScoreCalculator extends AbstractIncrementalScoreCalculator<Sol
 
     @Override
     public void beforeEntityRemoved(Object o) {
-
+        if(!(o instanceof Slide)){
+            return;
+        }
+        delete((Slide) o);
     }
 
     @Override
     public void afterEntityRemoved(Object o) {
-
+        // nothing
     }
 
     @Override
     public Score calculateScore() {
-        return null;
+        return HardSoftLongScore.of(0, softscore);
     }
+
+    private void insert(Slide s){
+        if(s.getPreviousRequestDelivered() instanceof Anchor) return;
+        softscore += FullScoreCalculator.getScore(s, (Slide) s.getPreviousRequestDelivered());
+    }
+
+    private void delete(Slide s){
+
+    }
+
+
 }
--- src/main/java/verolog/score/FullScoreCalculator.java
+++ src/main/java/verolog/score/FullScoreCalculator.java
@@ -17,21 +17,26 @@ public class FullScoreCalculator implements EasyScoreCalculator<Solution> {
         Slide slide = solution.getAnchors().get(0).getNextSlide();
         while(slide != null && slide.getNextSlide()!=null){
             Slide next = slide.getNextSlide();
-            int common = 0;
-            HashSet<String> thisTags = slide.getTags();
-            int soloThis = thisTags.size();
-            HashSet<String> nextTags = next.getTags();
-            int soloNext = nextTags.size();
-            for (var item: thisTags) {
-                if (nextTags.contains(item)) {
-                    common++;
-                    soloNext--;
-                    soloThis--;
-                }
-            }
-            softscore += Math.min(Math.min(soloNext,soloThis),common);
+            int score = getScore(slide, next);
+            softscore += score;
             slide = slide.getNextSlide();
         }
         return HardSoftLongScore.of(hardscore, softscore);
+    }
+
+    public static int getScore(Slide slide, Slide next) {
+        int common = 0;
+        HashSet<String> thisTags = slide.getTags();
+        int soloThis = thisTags.size();
+        HashSet<String> nextTags = next.getTags();
+        int soloNext = nextTags.size();
+        for (var item: thisTags) {
+            if (nextTags.contains(item)) {
+                common++;
+                soloNext--;
+                soloThis--;
+            }
+        }
+        return Math.min(Math.min(soloNext, soloThis), common);
     }
 }
